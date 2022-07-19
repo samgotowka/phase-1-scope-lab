f935a00cf08ddf1519e93a52c235fb52229cf7dd
Sam Gotowka <sam.gotowka@gmail.com>
2022-07-19 18:10:23 -0400

    commit tree 709e3e1c0a86fbd7504786b16720e100dd2b41c7

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavorite = 'barry';
+function upperCaseCustomerName() {
+   return customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+function leastFavoriteCustomer(){
+ return leastFavorite
+}
+function changeLeastFavoriteCustomer() {
+leastFavorite = 'Mike';
+}
